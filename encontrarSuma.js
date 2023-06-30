14ac2fed15428edbbb3f5e8433dac4ee7f366702
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2023-06-30 11:03:46 -0300

    Primer commit: cree el repositorio en Git

---
+++ encontrarSuma.js
@@ -0,0 +1,23 @@
+/**enfoque de fuerza bruta  */
+function encontrarSuma(array, X) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === X) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const array = [1, 3, 5, 7, 9];
+const X = 2;
+
+if (encontrarSuma(array, X)) {
+    console.log("Sí, es posible formar el número X usando la suma de 2 elementos en el array.");
+} else {
+    console.log("No es posible formar el número X usando la suma de 2 elementos en el array.");
+}
+
+/** El Algoritmo prueba todas las posibles combinaciones de pares de elementos del array y verifica si su suma es igual a X */
+/** puede volverse ineficiente para arrays grandes.*/
---
+++ encontrarSumaOptimizado.js
@@ -0,0 +1,33 @@
+/** enfoque basado en un conjunto (Set)  */
+function encontrarSuma(array, X) {
+    const complements = new Set();
+    for (let i = 0; i < array.length; i++) {
+        const complement = X - array[i];
+
+        if (complements.has(complement)) {
+            return true;
+        }
+
+        complements.add(array[i]);
+    }
+
+    return false;
+}
+
+const array = [10, 20, 40, 50, 80];
+const X = 130;
+
+if (encontrarSuma(array, X)) {
+    console.log("Sí, es posible formar el número X usando la suma de 2 elementos en el array.");
+} else {
+    console.log("No es posible formar el número X usando la suma de 2 elementos en el array.");
+}
+
+/**Utilice (Set) para almacenar los complementos necesarios 
+para formar X */
+/** evita la necesidad de comprobar todas las combinaciones posibles */
+/**calcula el complemento necesario para formar X  */
+/**Recorre, verifica y si encontró una combinación de 2
+elementos cuya suma es X, se devuelve verdadero. Sino el
+bucle retorna falso */
+/**tiene una complejidad de tiempo de O(n), donde n es el tamaño del array, ya que recorre el array una sola vez.  */
